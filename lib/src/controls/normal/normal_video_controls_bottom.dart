d85109c5daf7e2977fa9060b36d4a0c20b9f14e9
LiWeNHuI <sdgrlwh@163.com>
2023-03-15 17:11:56 +0800

    chore: 更换文件位置；

--- lib/src/controls/normal/normal_video_controls_bottom.dart
+++ lib/src/controls/normal/normal_video_controls_bottom.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_video_view/src/controls/base_controls.dart';
+import 'package:flutter_video_view/src/base_controls.dart';
 import 'package:flutter_video_view/src/video_config.dart';
 import 'package:flutter_video_view/src/video_view.dart';
 import 'package:flutter_video_view/src/widgets/widgets.dart';
